c9eda594622cec90b0ea114ab9368b82edeffcc9
Israel Calvete <icalvete@gmail.com>
2013-12-07 09:54:52 +0100

    Decoupling GlusterFS

--- manifests/glusterfs_server.pp
+++ manifests/glusterfs_server.pp
@@ -7,33 +7,39 @@ class roles::glusterfs_server (
 
 ) inherits roles {
 
+  package {'xfsprogs':
+      ensure => present,
+      before => Glusterfs::Brick[$volume_name]
+  }
+
+
   if ! $volume_name {
     fail('A GlusterFS cluster needs a volumen_name.')
   }
-  
+
   if ! $volume_data and ! is_hash($volume_data) {
     fail('A GlusterFS cluster needs some volumen data.')
   }
-  
+
   if ! $volume_mountpoint {
     fail('A GlusterFS cluster needs a volume mountpoint.')
   }
-  
+
   if ! $volume_type {
     fail('A GlusterFS cluster needs a volume type.')
   }
-  
+
   $peers  = keys($volume_data)
   $size   = size($peers)
   $bricks = glusterFunctions($volume_data, 'formatBricks')
 
   include glusterfs::server
-  
+
   glusterfs::brick {$volume_name:
     dev        => $volume_data[$::ipaddress_eth1]['dev'],
     mountpoint => $volume_data[$::ipaddress_eth1]['mountpoint'],
   }
-  
+
   glusterfs::peer::probe{$peers:
     require => Class['glusterfs::server'],
     before  => Glusterfs::Volume::Create[$volume_name]
