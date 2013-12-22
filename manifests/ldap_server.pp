5d46cbc21df794b8d2525191ad028d1b60e3cd4d
Israel Calvete <icalvete@gmail.com>
2013-12-22 17:18:14 +0100

    Adding ldap_server role

---
+++ manifests/ldap_server.pp
@@ -0,0 +1,26 @@
+class roles::ldap_server (
+  
+  $backup_local   = true,
+  $cluster        = false,
+  $cluster_peer   = undef,
+  $replication_id = undef
+
+) inherits roles {
+  
+  if $cluster {
+    if ! $cluster_peer {
+      fail('Cluster node needs a peer. Set cluster_peer param.')
+    }
+    if ! $replication_id {
+      fail('Cluster node needs replication_id param.')
+    }
+  }
+
+  class {'ds389':
+    backup_local   => $backup_local,
+    cluster        => $cluster,
+    cluster_peer   => $cluster_peer,
+    replication_id => $replication_id
+  }
+}
+
