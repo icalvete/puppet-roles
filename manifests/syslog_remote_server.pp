593acbef197b6301041170957f859144cd83e3aa
Israel Calvete <icalvete@gmail.com>
2014-01-06 16:01:59 +0000

    Change names

--- manifests/syslog_remote_server.pp
+++ manifests/syslog_remote_server.pp
@@ -1,8 +1,8 @@
 class roles::syslog_remote_server inherits roles {
 
   syslogng::load_conf{'syslog_receiver':
-    conf    => { source => '00receiver.conf.erb', target => '00sp_receiver.conf' },
-    log_dir => hiera('sp_log_dir'),
-    log     => hiera('sp_log'),
+    conf    => { source => '00receiver.conf.erb', target => '00receiver.conf' },
+    log_dir => hiera('syslog_log_dir'),
+    log     => hiera('syslog_log'),
   }
 }
