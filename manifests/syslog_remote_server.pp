0a44597819579b849757e224283cb1a61db7c9e2
Israel Calvete <icalvete@gmail.com>
2014-04-19 09:01:02 +0200

    Logs rotation

--- manifests/syslog_remote_server.pp
+++ manifests/syslog_remote_server.pp
@@ -1,5 +1,6 @@
 class roles::syslog_remote_server (
 
+  $root_log_dir = hiera('root_log_dir'),
   $log_dir      = hiera('sp_log_dir'),
   $log          = hiera('sp_log')
 
@@ -9,5 +10,11 @@ class roles::syslog_remote_server (
     conf    => { source => '00receiver.conf.erb', target => '00receiver.conf' },
     log_dir => $log_dir,
     log     => $log
+  }
+
+  logrotate::rule { 'org_logs':
+    path         => "${root_log_dir}/${sp_log_dir}/*",
+    rotate       => 7,
+    rotate_every => 'day',
   }
 }
