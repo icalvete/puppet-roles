ee00f7977beb0ecb6bb881d316fe633b284a52ae
Israel Calvete <icalvete@gmail.com>
2014-07-06 11:24:58 +0200

    port can be changed

--- manifests/memcached_server.pp
+++ manifests/memcached_server.pp
@@ -1,11 +1,13 @@
 class roles::memcached_server (
 
+  $port            = undef,
   $memory          = undef,
   $max_object_size = undef
 
 ) inherits roles {
 
     class {'memcached':
+      port            => $port,
       memory          => $memory,
       max_object_size => $max_object_size
     }
