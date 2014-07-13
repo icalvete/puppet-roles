fcc4eb5efc4b8c909cddc16d25a3ee480dafe395
Israel Calvete <icalvete@gmail.com>
2014-07-13 20:45:49 +0200

    http/https auth with apache

--- manifests/elasticsearch_server.pp
+++ manifests/elasticsearch_server.pp
@@ -9,11 +9,38 @@ class roles::elasticsearch_server (
   $repo_resource = 'elasticsearch-1.1.1.deb',
   $cluster       = undef,
   $jetty         = undef,
+  $apache        = undef,
+  $server_alias  = undef,
+  $kibana_server = undef,
   $template      = undef
 
 ) inherits roles {
 
   realize Package['curl']
+
+  if $apache {
+
+    include roles::apache2_server
+
+    apache2::module { 'proxy':
+      require => Class['roles::apache2_server']
+    }
+
+    apache2::module { 'proxy_http':
+      require => Class['roles::apache2_server']
+    }
+
+    $elasticsearch_htpasswd_file = hiera('htpasswd_file')
+    $elasticsearch_htpasswd_user = hiera('elasticsearch_user')
+    $elasticsearch_htpasswd_pass = hiera('elasticsearch_user_pass')
+
+    htpasswd::user {'elasticsearch_http_basic_user':
+      file     => $elasticsearch_htpasswd_file,
+      user     => $elasticsearch_htpasswd_user,
+      password => $elasticsearch_htpasswd_pass,
+      require  => Class['roles::apache2_server']
+    }
+  }
 
   class {'elasticsearch':
     repo_scheme   => $repo_scheme,
@@ -25,6 +52,9 @@ class roles::elasticsearch_server (
     repo_resource => $repo_resource,
     cluster       => $cluster,
     jetty         => $jetty,
+    apache        => $apache,
+    server_alias  => $server_alias,
+    kibana_server => $kibana_server,
     template      => $template
   }
 }
