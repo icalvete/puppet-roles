2dd6a89042352db3b2ab3bf106feb73ef6681998
Israel Calvete <icalvete@gmail.com>
2016-01-07 09:00:18 +0100

    Adding new parameters

--- manifests/td_agent_server.pp
+++ manifests/td_agent_server.pp
@@ -1,17 +1,19 @@
 class roles::td_agent_server (
 
   $config_template    = undef,
-  $elasticsearch_host = undef
+  $elasticsearch_host = undef,
+  $main_config        = undef,
 
 ) inherits roles {
 
   include fluentd
 
-  class {'fluentd::td-agent':
-    elasticsearch_host => $elasticsearch_host
+  class {'fluentd::td_agent':
+    elasticsearch_host => $elasticsearch_host,
+    main_config        => $main_config
   }
 
   if $config_template {
-    fluentd::td-agent::add_config{$config_template:}
+    fluentd::td_agent::add_config{$config_template:}
   }
 }
