122ff563153fc7a92fa836a6e0264a6a5e0799a0
Israel Calvete <icalvete@gmail.com>
2016-01-21 11:13:34 +0100

    Adding backup over AWS Glacier.

--- manifests/syslog_remote_server.pp
+++ manifests/syslog_remote_server.pp
@@ -2,9 +2,27 @@ class roles::syslog_remote_server (
 
   $root_log_dir = hiera('root_log_dir'),
   $log_dir      = hiera('sp_log_dir'),
-  $log          = hiera('sp_log')
+  $log          = hiera('sp_log'),
+  $glacier      = false,
+  $glacier_vault = undef
 
 ) inherits roles {
+
+  if $glacier == true and ! $glacier_vault {
+        fail('To save logs in glacier, a vault is required.')
+  }
+
+
+  if $glacier == true {
+    file { 'graphite_install_config':
+      ensure  => 'present',
+      path    => "/root/bin/glacier.sh",
+      content => template("${module_name}/syslog_remote_server/glacier.sh.erb"),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0775'
+    }
+  }
 
   syslogng::load_conf{'syslog_receiver':
     conf    => { source => '00receiver.conf.erb', target => '00receiver.conf' },
@@ -13,12 +31,14 @@ class roles::syslog_remote_server (
   }
 
   logrotate::rule { 'org_logs':
-    path         => "${root_log_dir}/${log_dir}/*.log",
-    rotate       => 7,
-    rotate_every => 'day',
-    compress     => true,
-    missingok    => true,
-    ifempty      => true
+    path          => "${root_log_dir}/${log_dir}/*.log",
+    rotate        => 7,
+    rotate_every  => 'day',
+    compress      => true,
+    missingok     => true,
+    ifempty       => true,
+    sharedscripts => true,
+    postrotate    => "/root/bin/glacier.sh ${glacier} > /dev/null"
   }
 
   logrotate::rule { 'apport':
