5b4c024f78241c21343a02d1db0db5906d47dd87
Israel Calvete <icalvete@gmail.com>
2016-02-02 15:53:40 +0100

    Since now this role uses the official elasticsearch module.

--- manifests/elasticsearch_server.pp
+++ manifests/elasticsearch_server.pp
@@ -1,74 +1,53 @@
 class roles::elasticsearch_server (
 
-  $repo_scheme   = 'http',
-  $repo_domain   = 'download.elasticsearch.org',
-  $repo_port     = false,
-  $repo_user     = false,
-  $repo_pass     = false,
-  $repo_path     = 'elasticsearch/elasticsearch',
-  $repo_resource = undef,
-  $cluster       = undef,
-  $jetty         = undef,
-  $apache        = undef,
-  $server_alias  = undef,
-  $kibana_server = undef,
-  $template      = undef,
-  $aws_access_key_id        = undef,
-  $aws_secret_access_key    = undef,
-  $aws_region               = undef,
-  $aws_bucket               = undef,
-  $backup = undef
+  $status           = 'enabled',
+  $manage_repo      = true,
+  $repo_version     = '2.x',
+  $version          = false,
+  $java_install     = true,
+  $cluster_name     = 'clustername',
+  $default_template = undef,
+  $default_script   = undef,
+  $bind_host        = $ipaddress,
+  $publish_host     = $ipaddress,
+  $hosts            = [$ipaddress]
 
 ) inherits roles {
 
-  realize Package['curl']
+  class { 'elasticsearch':
+    status                                   => $status,
+    manage_repo                              => $manage_repo,
+    repo_version                             => $repo_version,
+    version                                  => $version,
+    java_install                             => $java_install,
+    config                                   => {
+      'network.bind_host'                    => $bind_host,
+      'network.publish_host'                 => $publish_host,
+      'cluster.name'                         => $cluster_name,
+      'discovery.zen.ping.unicast.hosts'     => $hosts,
+      'discovery.zen.ping.multicast.enabled' => false,
+      'index.store.type'                     => 'niofs',
+      'index.store.compress.stored'          => true,
+      'bootstrap.mlockall'                   => true,
+      'http.compression'                     => true,
+      'transport.tcp.compress'               => true
+    },
+  }
 
-  if $apache {
+  #'node.name'                        => $hostname,
+  elasticsearch::instance { $hostname: }
 
-    include roles::apache2_server
-
-    if ! defined (Apache2::Module['proxy']) {
-      apache2::module { 'proxy':
-        require => Class['roles::apache2_server']
-      }
-    }
-
-    if ! defined (Apache2::Module['proxy_http']) {
-      apache2::module { 'proxy_http':
-        require => Class['roles::apache2_server']
-      }
-    }
-
-    $elasticsearch_htpasswd_file = hiera('htpasswd_file')
-    $elasticsearch_htpasswd_user = hiera('elasticsearch_user')
-    $elasticsearch_htpasswd_pass = hiera('elasticsearch_user_pass')
-
-    htpasswd::user {'elasticsearch_http_basic_user':
-      file     => $elasticsearch_htpasswd_file,
-      user     => $elasticsearch_htpasswd_user,
-      password => $elasticsearch_htpasswd_pass,
-      require  => Class['roles::apache2_server']
+  if  $default_template {
+    elasticsearch::template { 'elasticsearch_template':
+      ensure => 'present',
+      file   => $default_template
     }
   }
 
-  class {'elasticsearch':
-    repo_scheme           => $repo_scheme,
-    repo_domain           => $repo_domain,
-    repo_port             => $repo_port,
-    repo_user             => $repo_user,
-    repo_pass             => $repo_pass,
-    repo_path             => $repo_path,
-    repo_resource         => $repo_resource,
-    cluster               => $cluster,
-    jetty                 => $jetty,
-    apache                => $apache,
-    server_alias          => $server_alias,
-    kibana_server         => $kibana_server,
-    template              => $template,
-    aws_access_key_id     => $aws_access_key_id,
-    aws_secret_access_key => $aws_secret_access_key,
-    aws_region            => $aws_region,
-    aws_bucket            => $aws_bucket,
-    backup                => $backup
+  if $default_script {
+    elasticsearch::script { 'script_template':
+      ensure => 'present',
+      source => $default_script
+    }
   }
 }
