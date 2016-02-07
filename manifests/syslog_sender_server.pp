aa4e65c3b8df88d2d37693a53509d2d7c963712a
Israel Calvete <icalvete@gmail.com>
2016-02-07 17:14:58 +0100

    Improving syslog sender server role.

--- manifests/syslog_sender_server.pp
+++ manifests/syslog_sender_server.pp
@@ -1,13 +1,20 @@
 class roles::syslog_sender_server (
 
 $syslog_remote_server = undef,
-$syslog_remote_port   = undef
+$syslog_remote_port   = 514,
+$logstash_remote_server = undef,
+$logstash_remote_port   = undef
 
 ) inherits roles {
 
   syslogng::load_conf{'syslog_sender':
-    conf                 => { source => '00sender.conf.erb', target => '00sender.conf' },
-    syslog_remote_server => $syslog_remote_server,
-    syslog_remote_port   => $syslog_remote_port
+    conf                   => {
+      source => '00sender.conf.erb',
+      target => '00sender.conf'
+    },
+    syslog_remote_server   => $syslog_remote_server,
+    syslog_remote_port     => $syslog_remote_port,
+    logstash_remote_server => $logstash_remote_server,
+    logstash_remote_port   => $logstash_remote_port
   }
 }
