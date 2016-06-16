06b0234a4bc315bc06d92349913f8486246d2b51
Israel Calvete <icalvete@gmail.com>
2016-06-16 12:07:28 +0200

    Adding parse server role.

---
+++ manifests/parse_server.pp
@@ -0,0 +1,23 @@
+class roles::parse_server (
+
+  $application_id = undef,
+  $master_key     = undef,
+
+) inherits roles {
+
+  if $application_id == undef {
+    fail('application_id must be a string')
+  }
+
+  if $master_key == undef {
+    fail('master_key must be a string')
+  }
+
+   include nodejs
+
+   class {'parse_platform':
+    application_id => $application_id,
+    master_key     => $master_key,
+    require        => Class['nodejs']
+  }
+}
