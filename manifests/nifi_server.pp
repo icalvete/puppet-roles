88bd764a174341beebae1522e435244daaa0b9d0
Israel Calvete <icalvete@gmail.com>
2017-07-06 14:35:32 +0200

    Add auth by cert.

--- manifests/nifi_server.pp
+++ manifests/nifi_server.pp
@@ -1,12 +1,18 @@
 class roles::nifi_server (
 
-  $version       = undef,
-  $repo_scheme   = undef,
-  $repo_domain   = undef,
-  $repo_port     = false,
-  $repo_user     = false,
-  $repo_pass     = false,
-  $repo_resource = undef,
+  $version                = undef,
+  $repo_scheme            = undef,
+  $repo_domain            = undef,
+  $repo_port              = false,
+  $repo_user              = false,
+  $repo_pass              = false,
+  $repo_resource          = undef,
+  $auth                   = undef,
+  $keystore_file_source   = undef,
+  $keystore_password      = undef,
+  $admin                  = undef,
+  $truststore_file_source = undef,
+  $truststore_password    = undef,
 
 ) inherits roles {
 
@@ -15,13 +21,19 @@ class roles::nifi_server (
   }
 
   class {'nifi':
-    version       => $version,
-    repo_scheme   => $repo_scheme,
-    repo_domain   => $repo_domain,
-    repo_port     => false,
-    repo_user     => false,
-    repo_pass     => false,
-    repo_path     => $repo_path,
-    repo_resource => $repo_resource,
+    version                => $version,
+    repo_scheme            => $repo_scheme,
+    repo_domain            => $repo_domain,
+    repo_port              => false,
+    repo_user              => false,
+    repo_pass              => false,
+    repo_path              => $repo_path,
+    repo_resource          => $repo_resource,
+    auth                   => $auth,
+    keystore_file_source   => $keystore_file_source,
+    keystore_password      => $keystore_password,
+    admin                  => $admin,
+    truststore_file_source => $truststore_file_source,
+    truststore_password    => $truststore_password
   }
 }
