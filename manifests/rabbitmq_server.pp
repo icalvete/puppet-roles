86d9a3de7b4379735019deb8c5a304178d2cfefe
Israel Calvete <icalvete@gmail.com>
2017-11-07 16:09:47 +0100

    Add cluster support

--- manifests/rabbitmq_server.pp
+++ manifests/rabbitmq_server.pp
@@ -1,14 +1,29 @@
 class roles::rabbitmq_server (
 
-  $admin_user = 'admin',
-  $admin_pass = 'admin'
+  $admin_user               = 'admin',
+  $admin_pass               = 'admin',
+  $config_cluster           = false,
+  $cluster_node_type        = 'disc',
+  $cluster_nodes            = undef,
+  $erlang_cookie            = 'A_SECRET_COOKIE_STRING',
+  $wipe_db_on_cookie_change = true,
+
 
 ) inherits roles {
 
+  if $config_cluster and !$cluster_nodes {
+    fail('cluster_nodes must be an array if config_cluster is true')
+  }
+
   class { '::rabbitmq':
-    service_manage    => false,
-    port              => '5672',
-    delete_guest_user => true,
+    service_manage           => false,
+    port                     => '5672',
+    delete_guest_user        => true,
+    config_cluster           => $config_cluster,
+    cluster_node_type        => $cluster_node_type,
+    cluster_nodes            => $cluster_nodes,
+    erlang_cookie            => $erlang_cookie,
+    wipe_db_on_cookie_change => $wipe_db_on_cookie_change
   }
 
   rabbitmq_user { $admin_user:
